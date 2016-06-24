e54640b401c7dfa75fecb455c4ccf4ea8d64e992
Itay Banner <itay@ibanner.co.il>
2016-06-24 22:57:49 +0300

    Added The Loop

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
       <div class="row">
 
         <div class="col-sm-8 blog-main">
+			<?php 
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-			<?php get_template_part( 'content', get_post_format() ); ?>
+				get_template_part( 'content', get_post_format() );
+				
+			endwhile; endif; 
+			?>
 
           <nav>
             <ul class="pager">
