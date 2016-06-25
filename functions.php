7f26f7d9fca4f6191091a063b4b48b09604ca912
Itay Banner <itay@ibanner.co.il>
2016-06-26 00:04:10 +0300

    fixed enqueuing function

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 // Add scripts and stylesheets
-function startwordpress_scripts() {
+function wp-sandbox_scripts() {
 	wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css', array(), '3.3.6' );
 	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
 	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
