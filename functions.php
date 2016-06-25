882a1ef59f420dbc343c73420e285318993d2b16
Itay Banner <itay@ibanner.co.il>
2016-06-26 01:06:43 +0300

    enqueue style.css

--- functions.php
+++ functions.php
@@ -4,10 +4,13 @@
 function sandbox_scripts() {
 	wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css', array(), '3.3.6' );
 	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'sandbox_scripts' );
+
+
 
 // Add Google Fonts
 function sandbox_google_fonts() {
