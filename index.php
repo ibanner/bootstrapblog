6c69f769590de8cf9b553e8667cecd8b157865d9
Itay Banner <itay@ibanner.co.il>
2016-06-25 23:08:16 +0300

    added links to posts from the loop, removed static paging

--- index.php
+++ index.php
@@ -12,13 +12,6 @@
 			endwhile; endif; 
 			?>
 
-          <nav>
-            <ul class="pager">
-              <li><a href="#">Previous</a></li>
-              <li><a href="#">Next</a></li>
-            </ul>
-          </nav>
-
         </div><!-- /.blog-main -->
 
 		<?php get_sidebar(); ?>
