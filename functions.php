f6f59f25b80af1a1a89afe497b1b5b44a7c53394
Itay Banner <itay@ibanner.co.il>
2016-06-26 00:01:48 +0300

    added functions.php, enqueuing scripts and stylesheets

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+
+// Add scripts and stylesheets
+function startwordpress_scripts() {
+	wp_enqueue_style( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css', array(), '3.3.6' );
+	wp_enqueue_style( 'blog', get_template_directory_uri() . '/css/blog.css' );
+	wp_enqueue_script( 'bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js', array('jquery'), '3.3.6', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'wp-sandbox_scripts' );
