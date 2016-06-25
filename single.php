6a6b7a41f53de08b151c5b071e15b684f239dac1
Itay Banner <itay@ibanner.co.il>
2016-06-25 23:25:42 +0300

    added comments

--- single.php
+++ single.php
@@ -7,6 +7,10 @@
 				if ( have_posts() ) : while ( have_posts() ) : the_post();
   	
 					get_template_part( 'content-single', get_post_format() );
+					
+						if ( comments_open() || get_comments_number() ) :
+						  comments_template();
+						endif;
   
 				endwhile; endif; 
 			?>
