3fca6e273d2ed2d9db8bdc57dd5f439cdad778eb
Itay Banner <itay@ibanner.co.il>
2016-06-26 16:05:45 +0300

    showing IATA code after the title

--- page-airports.php
+++ page-airports.php
@@ -13,8 +13,7 @@
             while ($custom_query->have_posts()) : $custom_query->the_post(); ?>
 
 				<div class="blog-post airport-cpt">
-					<h2 class="blog-post-title airport-cpt-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-					<?php echo get_post_meta( get_the_ID(), 'meta-box-text' , true ); ?>
+					<h2 class="blog-post-title airport-cpt-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> <?php echo $airport_iata_code; ?> </h2>
 					<?php the_excerpt(); ?>
 				</div>
 
