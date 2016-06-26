a30560e7c35d97b3f93cba4097a839adcf7bb751
Itay Banner <itay@ibanner.co.il>
2016-06-26 22:29:24 +0300

    added content-airport, single-airport

--- single-airport.php
+++ single-airport.php
@@ -1,18 +1,16 @@
 <?php get_header(); ?>
-page.php
-
+single-airport.php
 	<div class="row">
 		<div class="col-sm-12">
 
 			<?php 
 				if ( have_posts() ) : while ( have_posts() ) : the_post();
   	
-					get_template_part( 'content', get_post_format() );
+					get_template_part( 'content-airport', get_post_format() );
   
 				endwhile; endif; 
 			?>
 
-		</div> <!-- /.col -->
 	</div> <!-- /.row -->
 
 <?php get_footer(); ?>
