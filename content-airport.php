537936cf3bae2fdf4bb4784ae7802be6ddaad8b9
Itay Banner <itay@ibanner.co.il>
2016-06-26 08:36:46 +0300

    added content-airport

---
+++ content-airport.php
@@ -0,0 +1,11 @@
+<div class="blog-post">
+	<h2 class="blog-post-title"><?php the_title(); ?></h2>
+	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+
+	<?php if ( has_post_thumbnail() ) {
+	  the_post_thumbnail();
+	} ?>
+	<?php echo get_post_meta( get_the_ID(), 'meta-box-text' , true ); ?>
+	<?php the_content(); ?>
+
+</div><!-- /.blog-post -->
