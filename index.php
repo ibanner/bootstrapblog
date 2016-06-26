78c864d7bd83c6f66edfd66b882e1222867259ab
Itay Banner <itay@ibanner.co.il>
2016-06-26 16:55:11 +0300

    setting posts to have sidebar again

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-
 <?php get_header(); ?>
 
       <div class="row">
-
         <div class="col-sm-8 blog-main">
 			<?php 
 			if ( have_posts() ) : while ( have_posts() ) : the_post();
--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 	<div class="row">
-		<div class="col-sm-12">
+		<div class="col-sm-8 blog-main">
 
 			<?php 
 				if ( have_posts() ) : while ( have_posts() ) : the_post();
@@ -15,7 +15,10 @@
 				endwhile; endif; 
 			?>
 
-		</div> <!-- /.col -->
+		</div><!-- /.blog-main -->
+
+		<?php get_sidebar(); ?>
+
 	</div> <!-- /.row -->
 
 <?php get_footer(); ?>
