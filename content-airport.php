0165e8a3d50c1b0a149b268ab21c0efbfed73770
Itay Banner <itay@ibanner.co.il>
2016-06-26 22:34:09 +0300

    added IATA code to single-airport title

--- content-airport.php
+++ content-airport.php
@@ -1,10 +1,10 @@
-<div class="blog-post">
-	<h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+<div class="blog-post airport">
+	<h2 class="blog-post-title airport-title"><?php the_title(); ?> (<?php echo get_post_meta( get_the_ID(), 'airport_iata_code' , true); ?>) </h2>
+	<p class="blog-post-meta airport-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
 	<?php if ( has_post_thumbnail() ) {
 	  the_post_thumbnail();
 	} ?>
 	<?php the_content(); ?>
 
-</div><!-- /.blog-post -->
+</div><!-- /.blog-post /.airport -->
