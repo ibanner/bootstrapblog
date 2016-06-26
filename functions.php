aea890960caeb1722f6b97587e0d48416bd53299
Itay Banner <itay@ibanner.co.il>
2016-06-26 06:29:57 +0300

    a fuller CPT function with lotsa labels

--- functions.php
+++ functions.php
@@ -82,21 +82,56 @@ add_action( 'admin_init', 'custom_settings_page_setup' );
 // ++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // 3.1 Declare Airport as a Custom Post Type
+
 function create_airport() {
-	register_post_type('sandbox_airport',
-			array(
-			'labels' => array(
-					'name' => __('Airport'),
-					'singular_name' => __('Airport'),
-			),
-			'public' => true,
-			'has_archive' => true,
-			'supports' => array(
-					'title',
-					'editor',
-					'thumbnail',
-				  'custom-fields'
-			)
-	));
+
+	$labels = array(
+		'name'                  => _x( 'Airports', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Airport', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Airports', 'text_domain' ),
+		'name_admin_bar'        => __( 'Airport', 'text_domain' ),
+		'archives'              => __( 'Airport Archives', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Airport:', 'text_domain' ),
+		'all_items'             => __( 'All Airports', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Airport', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Airport', 'text_domain' ),
+		'edit_item'             => __( 'Edit Airport', 'text_domain' ),
+		'update_item'           => __( 'Update Airport', 'text_domain' ),
+		'view_item'             => __( 'View Airport', 'text_domain' ),
+		'search_items'          => __( 'Search Airport', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+		'items_list'            => __( 'Airports list', 'text_domain' ),
+		'items_list_navigation' => __( 'Airports list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter Airports list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Airport', 'text_domain' ),
+		'description'           => __( 'Airport Description', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'sandbox_airport', $args );
+
 }
-add_action('init', 'create_airport');
+add_action( 'init', 'create_airport', 0 );
